c9bba1fe2ff7765ee08146ebd4b34ede36d9f0b8
camargo <matheuscamarrgo@gmail.com>
2025-01-24 18:05:52 -0300

    exemplo basico de como enviar uma mensagem automatica pela api e cron job

---
+++ wpAPITest.php
@@ -0,0 +1,41 @@
+<?php
+
+// no cron do servidor add * * * * *	curl -s https://projetos.agendarhora.com/api/nomedaapi.php
+
+$url = "https://graph.facebook.com/v21.0/544155972112980/messages";
+$accessToken = "addaquiseutoken"; // Substitua pelo seu token de acesso.
+
+$data = array(
+    "messaging_product" => "whatsapp",
+    "to" => "5541987493925", // Substitua pelo número do destinatário.
+    "type" => "template",
+    "template" => array(
+        "name" => "hello_world",
+        "language" => array(
+            "code" => "en_US"
+        )
+    )
+);
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    "Authorization: Bearer $accessToken",
+    "Content-Type: application/json"
+));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+
+$response = curl_exec($ch);
+
+if (curl_errno($ch)) {
+    echo 'Erro: ' . curl_error($ch);
+} else {
+    echo 'Resposta: ' . $response;
+}
+
+curl_close($ch);
+
+?>
